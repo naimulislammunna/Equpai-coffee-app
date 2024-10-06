40fb2de4cec269703ca488b74ff624502a50aadc
naimulislammunna <naimulislammunna742@gmail.com>
2024-10-07 02:42:12 +0600

    added

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -1,6 +1,14 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import Product from "./Product";
 
 const Products = () => {
+    const [items, setItems] = useState([]);
+    useEffect(() =>{
+        fetch('http://localhost:4000/coffee-items')
+        .then(res => res.json())
+        .then(data => setItems(data))
+    }, [])
     return (
         <div className="home-container">
             <h1 className="text-4xl text-center font-semibold">Our Popular Products</h1>
@@ -8,7 +16,9 @@ const Products = () => {
                 <Link to='/add-coffee'><button className="bg-[#E3B577] border border-gray-950 px-3 py-1">Add Coffee</button></Link>
             </div>
             <div>
-
+                {
+                    items?.map((item, idx) => <Product key={idx} item={item}></Product>)
+                }
             </div>
         </div>
     );
