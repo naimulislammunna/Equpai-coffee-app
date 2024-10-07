a353c0608c5cc9c719debbea439b6367f3b22fcd
naimulislammunna <naimulislammunna742@gmail.com>
2024-10-08 02:16:39 +0600

    get coffee data

--- src/Pages/Products/Product.jsx
+++ src/Pages/Products/Product.jsx
@@ -3,32 +3,71 @@ import { IoMdEye } from "react-icons/io";
 import { MdDelete } from "react-icons/md";
 import useUrl from "../../Hooks/useUrl";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
 
-const Product = ({ item }) => {
+const Product = ({ item, delCount }) => {
     const url = useUrl();
+    const [deleteCount, setDeleteCount] = useState(false);
+    const [coffee, setCoffee] = useState([]);
+    delCount(deleteCount)
+    const handleDelete = async (id) => {
+        const response = await axios.delete(`${url}/delete/${id}`);
+        if (response.data?.deletedCount > 0) {
+            setDeleteCount(true);
+        }
 
+    }
+    const handleShow = async (id) => {
+        const response = await axios(`${url}/coffee-item/${id}`);
+        setCoffee(response?.data);
 
-    
+    }
+
     return (
-        <div className="bg-base-200 rounded-xl px-5 py-4">
-            <div className="flex gap-5 justify-between items-center">
-                <div>
-                    <img src={item?.photoUrl} alt="" className="w-[140px]" />
-                </div>
-                <div className="space-y-2 text-xl">
-                    <p>Name: {item?.name}</p>
-                    <p>Chef: {item?.chef}</p>
-                    <p>Price: ${item?.price}</p>
-                </div>
-                <div className="space-y-2">
-                    <Link to={`/update/${item._id}`}>
-                        <p className="bg-[#E3B577] p-2 rounded-md text-white cursor-pointer"><span><MdEdit /></span></p>
-                    </Link>
-                    <p onClick={() => handleDetails(item._id)} className="bg-[#5C5B5B]  p-2 rounded-md text-white cursor-pointer"><span><IoMdEye /></span></p>
-                    <p onClick={() => handleDelete(item._id)} className="bg-[#EA4744]  p-2 rounded-md text-white cursor-pointer"><span><MdDelete /></span></p>
+        <>
+            <div className="bg-base-200 rounded-xl px-5 py-4">
+                <div className="flex gap-5 justify-between items-center">
+                    <div>
+                        <img src={item?.photoUrl} alt="" className="w-[140px]" />
+                    </div>
+                    <div className="space-y-2 text-xl">
+                        <p>Name: {item?.name}</p>
+                        <p>Chef: {item?.chef}</p>
+                        <p>Price: ${item?.price}</p>
+                    </div>
+                    <div className="space-y-2">
+                        <Link to={`/update/${item._id}`}>
+                            <p className="bg-[#E3B577] p-2 rounded-md text-white cursor-pointer"><span><MdEdit /></span></p>
+                        </Link>
+                        <p onClick={() => { document.getElementById('my_modal_3').showModal(); handleShow(item._id) }} className="bg-[#5C5B5B]  p-2 rounded-md text-white cursor-pointer"><span><IoMdEye /></span></p>
+                        <p onClick={() => handleDelete(item._id)} className="bg-[#EA4744]  p-2 rounded-md text-white cursor-pointer"><span><MdDelete /></span></p>
+                    </div>
                 </div>
             </div>
-        </div>
+            {/* You can open the modal using document.getElementById('ID').showModal() method */}
+            <dialog id="my_modal_3" className="modal">
+                <div className="modal-box bg-base-200 rounded-xl">
+                    <form method="dialog">
+                        {/* if there is a button in form, it will close the modal */}
+                        <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                    </form>
+                    <div className="flex gap-20 items-center px-5 py-4">
+                        <div>
+                            <img src={coffee?.photoUrl} alt="" className="w-[140px]" />
+                        </div>
+                        <div className="space-y-2 text-xl">
+                            <p>Name: {coffee?.name}</p>
+                            <p>Chef: {coffee?.chef}</p>
+                            <p>Price: ${coffee?.price}</p>
+                            <p>Supplier: {coffee?.supplier}</p>
+                            <p>Teste: {coffee?.teste}</p>
+                            <p>Category: {coffee?.category}</p>
+                        </div>
+                    </div>
+                </div>
+            </dialog>
+        </>
     );
 };
 
--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -4,11 +4,17 @@ import Product from "./Product";
 
 const Products = () => {
     const [items, setItems] = useState([]);
+    const [del, setDel] = useState([]);
+
+    const delCount = (idx) =>{
+        setDel(idx)
+    }
     useEffect(() => {
         fetch('http://localhost:4000/coffee-items')
             .then(res => res.json())
             .then(data => setItems(data))
-    }, [])
+    }, [del])
+    
     return (
         <div className="home-container">
             <div className="text-center mt-5">
@@ -16,7 +22,7 @@ const Products = () => {
                 <Link to='/add-coffee'><button className="bg-[#E3B577] border border-gray-950 px-3 py-1 my-4">Add Coffee</button></Link>
                 <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 my-5">
                     {
-                        items?.map((item, idx) => <Product key={idx} item={item}></Product>)
+                        items?.map((item, idx) => <Product key={idx} item={item} delCount={delCount}></Product>)
                     }
                 </div>
             </div>
